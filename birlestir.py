f120dcf94328654db0ab409ebec0bec944715b78
MDuymazz <duymaz.mrt@gmail.com>
2025-03-25 19:55:25 +0300

    Update birlestir.py

--- birlestir.py
+++ birlestir.py
@@ -31,11 +31,15 @@ def sort_m3u(lines):
         sorted_lines.extend(entry)
     return sorted_lines
 
-def merge_m3u(url1, url2, output_file="playlist.m3u"):
+def merge_m3u(url1, url2, url3, output_file="playlist.m3u"):
+    new_m3u = download_m3u(url3)  # En üste eklenecek dosya
     gol_m3u = download_m3u(url1)
     vavoo_m3u = download_m3u(url2)
     
     merged_content = ["#EXTM3U"]
+    
+    if new_m3u and new_m3u[0] == "#EXTM3U":
+        new_m3u = new_m3u[1:]
     
     sorted_gol = sort_m3u(gol_m3u)
     if sorted_gol and sorted_gol[0] == "#EXTM3U":
@@ -44,6 +48,8 @@ def merge_m3u(url1, url2, output_file="playlist.m3u"):
     if vavoo_m3u and vavoo_m3u[0] == "#EXTM3U":
         vavoo_m3u = vavoo_m3u[1:]
     
+    # new_m3u en üste yazılacak
+    merged_content.extend(new_m3u)
     merged_content.extend(sorted_gol)
     merged_content.extend(vavoo_m3u)
     
@@ -52,5 +58,6 @@ def merge_m3u(url1, url2, output_file="playlist.m3u"):
 
 merge_m3u(
     "https://raw.githubusercontent.com/MDuymazz/sitem3u/refs/heads/main/gol.m3u",
-    "https://raw.githubusercontent.com/MDuymazz/efendikaptan/refs/heads/main/vavoo.m3u"
+    "https://raw.githubusercontent.com/MDuymazz/efendikaptan/refs/heads/main/vavoo.m3u",
+    "https://raw.githubusercontent.com/MDuymazz/efendikaptan/refs/heads/main/new_m3u.m3u"
 )
